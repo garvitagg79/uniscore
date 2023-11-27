8764a925eb268095691225140fc809a6e738ca6a
garvitagg79 <81882241+garvitagg79@users.noreply.github.com>
2023-11-27 18:35:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,8 @@ const calculateScore = (parsedScore, average, sd) => {
   if (normal > -0.5 && normal <= 0.0) return 6;
   if (normal > -1.0 && normal <= -0.5 && parsedScore >= 70) return 6;
   if (normal > -1.0 && normal <= -0.5 && parsedScore < 70) return 5;
-  if (normal > -1.5 && normal <= -1.0) return 4;
+  if (normal > -1.5 && normal <= -1.0 && parsedScore >= 60) return 5;
+  if (normal > -1.5 && normal <= -1.0 && parsedScore < 60) return 4;
   // Add more conditions as needed
   if (parsedScore > 54) return 4;
   return 0; // Default score if none of the conditions are met
